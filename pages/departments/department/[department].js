c775049b23043caea116a66389f85ee52d600f76
sphantom99 <georgeal1999@gmail.com>
2021-03-26 10:55:00 +0200

    Fixed #1

---
+++ pages/departments/department/[department].js
@@ -0,0 +1,77 @@
+import { Table, Tag, Space } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+export default function allEmployeesInDepartment(){
+
+  const columns = [
+    { title: 'Name', dataIndex: 'name', key: 'name' },
+    { title: 'Age', dataIndex: 'age', key: 'age' },
+    { title: 'Address', dataIndex: 'address', key: 'address' },
+    {
+      title: 'Action',
+      dataIndex: '',
+      key: 'x',
+      render: () => <a>Delete</a>,
+    },
+  ];
+  const data = [
+    {
+      key: 1,
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+    },
+    {
+      key: 2,
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+    },
+    {
+      key: 3,
+      name: 'Not Expandable',
+      age: 29,
+      address: 'Jiangsu No. 1 Lake Park',
+      description: 'This not expandable',
+    },
+    {
+      key: 4,
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+    },
+  ];
+const { Column, ColumnGroup } = Table;
+const { Header, Content, Footer } = Layout;
+
+
+return(
+    <div>
+    <Layout className="layout">
+    <Header>
+  <div className="logo" />
+  <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+    <Menu.Item key="1">nav 1</Menu.Item>
+    <Menu.Item key="2">nav 2</Menu.Item>
+    <Menu.Item key="3">nav 3</Menu.Item>
+  </Menu>
+    </Header>
+<Content style={{ padding: '0 50px' }}>
+<Table
+    columns={columns}
+    expandable={{
+      expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
+      rowExpandable: record => record.name !== 'Not Expandable',
+    }}
+    dataSource={data}
+  />
+  </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
+    </div>
+)
+}
